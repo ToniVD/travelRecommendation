e356bcc1cbd7a3456beb8f494f194be4de6936d9
ToniVD <vividrakes2147@gmail.com>
2024-06-25 14:10:24 -0400

    update5

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,30 +2,63 @@ const clearSearchButton = document.getElementById("btnClear");
 const searchButton = document.getElementById("btnSearch");
 
 function searchRecommendations(){
-    const input = document.getElementById('searchInput').value.toLowerCase();
-    // const resultDiv = document.getElementById('recommendation');
-    // resultDiv.innerHTML = '';
+    const input = document.getElementById('searchInput').value.toLowerCase().trim();
+    const recommendationDiv = document.getElementById('recommendation');
+    recommendationDiv.innerHTML = '';
 
     fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
-                if (input === "beach" || input === "beaches") {
+        if (input === "beach" || input === "beaches") {
                     
-                    for (let i = 0; i < data.beaches.length; i++){
-                        let obj = data.beaches[i].name
-                        console.log(obj)
+            for (let i = 0; i < 2; i++){
+                let obj = data.beaches[i]
+                console.log(obj.name)
+                recommendationDiv.innerHTML += `<h3>${obj.name}</h3>`;
+                recommendationDiv.innerHTML += `<img src="${obj.imageUrl}" alt="hjh">`;
+                recommendationDiv.innerHTML += `<p>${obj.description}</p>`;
+            }
+        }
+
+        if (input === "temple" || input === "temples") {
+            
+            for (let i = 0; i < 2; i++){
+                let obj = data.temples[i]
+                console.log(obj.name)
+                recommendationDiv.innerHTML += `<h3>${obj.name}</h3>`;
+                recommendationDiv.innerHTML += `<img src="${obj.imageUrl}" alt="hjh">`;
+                recommendationDiv.innerHTML += `<p>${obj.description}</p>`;
+
+            }
+        }
+
+        if (input === "country" || input === "countries") {
+            
+            for (let i = 0; i < 2; i++){
+                let cities = data.countries[i].cities
+                    for (let j = 0; j< 1; j++){
+                        console.log(cities[j].name)
+                        let obj = cities[i]
+                        recommendationDiv.innerHTML += `<h3>${obj.name}</h3>`;
+                        recommendationDiv.innerHTML += `<img src="${obj.imageUrl}" alt="hjh">`;
+                        recommendationDiv.innerHTML += `<p>${obj.description}</p><br>`;
                     }
-                }
+            }
+        }
       })
       .catch(error => {
         console.error('Error:', error);
       });
 }
 
-// clearSearchButton.addEventListener('click', clearSearch);
+
 
 searchButton.addEventListener('click', searchRecommendations);
 
-// function clearSearch() {
-//    // document.getElementById("wordInput").value = "";
-// }
+function clearSearch() {
+    const recommendationDiv = document.getElementById('recommendation');
+    recommendationDiv.innerHTML = '';
+    document.getElementById("searchInput").value = "";
+}
+
+clearSearchButton.addEventListener('click', clearSearch);
