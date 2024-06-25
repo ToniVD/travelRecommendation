ca3026ed1e38b89997d69c9a0b740d6c57f5630d
ToniVD <vividrakes2147@gmail.com>
2024-06-25 09:44:57 -0400

    update4

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,31 +2,30 @@ const clearSearchButton = document.getElementById("btnClear");
 const searchButton = document.getElementById("btnSearch");
 
 function searchRecommendations(){
-    const input = document.getElementById('wordInput').value.toLowerCase();
-    const resultDiv = document.getElementById('recommendation');
-    resultDiv.innerHTML = '';
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    // const resultDiv = document.getElementById('recommendation');
+    // resultDiv.innerHTML = '';
 
-    fetch('health_analysis.json')
+    fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
-        const word = data.words.find(item => item.name.toLowerCase() === input);
-        // use || (OR) for find like || data.temples.name.find()
-        // || data.countries.name.cities.name.find() etc
-        // Task 7: if beach || temple || country
-        // find in? like if they type australia, that should bring up
-        // australia sydney or melbourne
-
+                if (input === "beach" || input === "beaches") {
+                    
+                    for (let i = 0; i < data.beaches.length; i++){
+                        let obj = data.beaches[i].name
+                        console.log(obj)
+                    }
+                }
       })
+      .catch(error => {
+        console.error('Error:', error);
+      });
 }
 
-clearSearchButton.addEventListener('click', clearSearch);
+// clearSearchButton.addEventListener('click', clearSearch);
 
-searchButton.addEventListener('click', searchRecommendation);
+searchButton.addEventListener('click', searchRecommendations);
 
-function clearSearch() {
-   // document.getElementById("wordInput").placeholder = "";
-}
-
-function searchRecommendation() {
-    
-}
+// function clearSearch() {
+//    // document.getElementById("wordInput").value = "";
+// }
